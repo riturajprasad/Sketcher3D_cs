6cc2240ff277813caa64f96c955cfbe59be5268b
Rituraj Prasad <rituraj.prasad2718@gmail.com>
2025-12-15 11:44:16 +0530

    Complete DLL

--- GeometryEngine3D/Matrix.cs
+++ GeometryEngine3D/Matrix.cs
@@ -23,7 +23,7 @@ namespace GeometryEngine3D
             get { return mData[r, c]; }
             set { mData[r, c] = value; }
         }
-        public Matrix getIdentity(int n = 4)
+        public static Matrix getIdentity(int n = 4)
         {
             Matrix identity = new Matrix(n, n);
             for (int i = 0; i < n; i++) identity[i, i] = 1;
@@ -60,6 +60,55 @@ namespace GeometryEngine3D
                 return result;
             }
             else return null;
+        }
+        public static Matrix getTranslationMatrix(double tx = 0, double ty = 0, double tz = 0)
+        {
+            Matrix transMat = getIdentity();
+            transMat[0, 3] += tx;
+            transMat[1, 3] += ty;
+            transMat[2, 3] -= tz;
+            return transMat;
+        }
+        public static Matrix getScalingMatrix(double sx = 1, double sy = 1, double sz = 1)
+        {
+            Matrix scaleMat = getIdentity();
+            scaleMat[0, 0] = sx;
+            scaleMat[1, 1] = sy;
+            scaleMat[2, 2] = sz;
+            return scaleMat;
+        }
+        public static Matrix getRotationXMatrix(double degreeX)
+        {
+            Matrix rotXMat = getIdentity();
+            double radAngX = degreeX * Math.PI / 180.0;
+            double cosX = Math.Cos(radAngX);
+            double sinX = Math.Sin(radAngX);
+
+            rotXMat[1, 1] = cosX; rotXMat[1, 2] = -sinX;
+            rotXMat[2, 1] = sinX; rotXMat[2, 2] = cosX;
+            return rotXMat;
+        }
+        public static Matrix getRotationYMatrix(double degreeY)
+        {
+            Matrix rotYMat = getIdentity();
+            double radAngY = degreeY * Math.PI / 180.0;
+            double cosY = Math.Cos(radAngY);
+            double sinY = Math.Sin(radAngY);
+
+            rotYMat[0, 0] = cosY; rotYMat[0, 2] = sinY;
+            rotYMat[2, 0] = -sinY; rotYMat[2, 2] = cosY;
+            return rotYMat;
+        }
+        public static Matrix getRotationZMatrix(double degreeZ)
+        {
+            Matrix rotZMat = getIdentity();
+            double radAngZ = degreeZ * Math.PI / 180.0;
+            double cosZ = Math.Cos(radAngZ);
+            double sinZ = Math.Sin(radAngZ);
+
+            rotZMat[0, 0] = cosZ; rotZMat[0, 1] = -sinZ;
+            rotZMat[1, 0] = sinZ; rotZMat[1, 1] = cosZ;
+            return rotZMat;
         }
     }
 }
---
+++ GeometryEngine3D/Transformations.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryEngine3D
+{
+    internal class Transformations
+    {
+        public static List<Point> applyTransform(List<Point> vertices, Matrix matrix)
+        {
+            List<Point> transformedPts = new List<Point>();
+            foreach (Point p in vertices)
+            {
+                Matrix pt = new Matrix(4, 1);
+                pt[0, 0] = p.getX();
+                pt[1, 0] = p.getY();
+                pt[2, 0] = p.getZ();
+                pt[3, 0] = 1.0;
+
+                Matrix result = matrix * pt;
+                double X = result[0, 0];
+                double Y = result[1, 0];
+                double Z = result[2, 0];
+                Point resultPt = new Point(X, Y, Z);
+                transformedPts.Add(resultPt);
+            }
+            return transformedPts;
+        }
+        public static List<float> translate(List<float> vec, double transX, double transY, double transZ)
+        {
+            List<Point> vertices = new List<Point>();
+            for (int i = 0; i < vec.Count(); i += 3)
+            {
+                Point vecPt = new Point(vec[i], vec[i+1], vec[i+2]);
+                vertices.Add(vecPt);
+            }
+            Matrix transMat = Matrix.getTranslationMatrix(transX, transY, transZ);
+            List<Point> transformedPts = applyTransform(vertices, transMat);
+
+            for (int i = 0; i < transformedPts.Count(); i++)
+            {
+                vec.Add((float)transformedPts[i].getX());
+                vec.Add((float)transformedPts[i].getY());
+                vec.Add((float)transformedPts[i].getZ());
+            }
+            return vec;
+        }
+        public static List<float> scale(List<float> vec, double scaleX, double scaleY, double scaleZ)
+        {
+            List<Point> vertices = new List<Point>();
+            for (int i = 0; i < vec.Count(); i += 3)
+            {
+                Point vecPt = new Point(vec[i], vec[i+1], vec[i+2]);
+                vertices.Add(vecPt);
+            }
+            Point pivot = new Point();
+            Matrix translate1 = Matrix.getTranslationMatrix(-pivot.getX(), -pivot.getY(), -pivot.getY());
+            Matrix scaleMat = Matrix.getScalingMatrix(scaleX, scaleY, scaleZ);
+            Matrix translate2 = Matrix.getTranslationMatrix(pivot.getX(), pivot.getY(), pivot.getY());
+
+            Matrix transformed = translate2 * scaleMat * translate1;
+            List<Point> transformedPts = applyTransform(vertices, transformed);
+
+            for (int i = 0; i < transformedPts.Count(); i++)
+            {
+                vec.Add((float)transformedPts[i].getX());
+                vec.Add((float)transformedPts[i].getY());
+                vec.Add((float)transformedPts[i].getZ());
+            }
+            return vec;
+        }
+        public static List<float> rotationX(List<float> vec, double degreeX)
+        {
+            List<Point> vertices = new List<Point>();
+            for (int i = 0; i < vec.Count(); i += 3)
+            {
+                Point vecPt = new Point(vec[i], vec[i+1], vec[i+2]);
+                vertices.Add(vecPt);
+            }
+            Point pivot = new Point();
+            Matrix translate1 = Matrix.getTranslationMatrix(-pivot.getX(), -pivot.getY(), -pivot.getY());
+            Matrix rotateXMat = Matrix.getRotationXMatrix(degreeX);
+            Matrix translate2 = Matrix.getTranslationMatrix(pivot.getX(), pivot.getY(), pivot.getY());
+
+            Matrix transformed = translate2 * rotateXMat * translate1;
+            List<Point> transformedPts = applyTransform(vertices, transformed);
+
+            for (int i = 0; i < transformedPts.Count(); i++)
+            {
+                vec.Add((float)transformedPts[i].getX());
+                vec.Add((float)transformedPts[i].getY());
+                vec.Add((float)transformedPts[i].getZ());
+            }
+            return vec;
+        }
+        public static List<float> rotationY(List<float> vec, double degreeY)
+        {
+            List<Point> vertices = new List<Point>();
+            for (int i = 0; i < vec.Count(); i += 3)
+            {
+                Point vecPt = new Point(vec[i], vec[i+1], vec[i+2]);
+                vertices.Add(vecPt);
+            }
+            Point pivot = new Point();
+            Matrix translate1 = Matrix.getTranslationMatrix(-pivot.getX(), -pivot.getY(), -pivot.getY());
+            Matrix rotateYMat = Matrix.getRotationYMatrix(degreeY);
+            Matrix translate2 = Matrix.getTranslationMatrix(pivot.getX(), pivot.getY(), pivot.getY());
+
+            Matrix transformed = translate2 * rotateYMat * translate1;
+            List<Point> transformedPts = applyTransform(vertices, transformed);
+
+            for (int i = 0; i < transformedPts.Count(); i++)
+            {
+                vec.Add((float)transformedPts[i].getX());
+                vec.Add((float)transformedPts[i].getY());
+                vec.Add((float)transformedPts[i].getZ());
+            }
+            return vec;
+        }
+        public static List<float> rotationZ(List<float> vec, double degreeZ)
+        {
+            List<Point> vertices = new List<Point>();
+            for (int i = 0; i < vec.Count(); i += 3)
+            {
+                Point vecPt = new Point(vec[i], vec[i+1], vec[i+2]);
+                vertices.Add(vecPt);
+            }
+            Point pivot = new Point();
+            Matrix translate1 = Matrix.getTranslationMatrix(-pivot.getX(), -pivot.getY(), -pivot.getY());
+            Matrix rotateZMat = Matrix.getRotationZMatrix(degreeZ);
+            Matrix translate2 = Matrix.getTranslationMatrix(pivot.getX(), pivot.getY(), pivot.getY());
+
+            Matrix transformed = translate2 * rotateZMat * translate1;
+            List<Point> transformedPts = applyTransform(vertices, transformed);
+
+            for (int i = 0; i < transformedPts.Count(); i++)
+            {
+                vec.Add((float)transformedPts[i].getX());
+                vec.Add((float)transformedPts[i].getY());
+                vec.Add((float)transformedPts[i].getZ());
+            }
+            return vec;
+        }
+    }
+}
